ea538927b3c9e151fde71c4d403ae7cd863a4414
Akash Manohar J <akash@akash.im>
2011-07-30 23:35:32 +0530

    removed tree from app js
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/github_api_controller.rb
+++ app/controllers/github_api_controller.rb
@@ -5,4 +5,13 @@ class GithubApiController < ApplicationController
   def repo_commits
   end
 
+  def repo_info
+  end
+
+  # get images of all github commiters
+  def repo_committers_info
+  end
+
+  
+
 end
