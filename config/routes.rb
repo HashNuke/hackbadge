c27f9fbfaa56f7b0758c04634cfc9717dd301e83
Akash Manohar J <akash@akash.im>
2011-07-30 13:18:03 +0530

    initial setup done
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Hackbadge::Application.routes.draw do
+
+  match "/auth/:provider/callback" => "sessions#create"
+  match "/logout" => "sessions#destroy", :as => :logout
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
